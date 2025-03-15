b4ae0dd0273761f6fabcb99ee6ecb4284d0c6515
ndjia <nicolep0522@gmail.com>
2025-03-15 11:46:04 -0400

    added library StackingSceneMgr.lua

--- main.lua
+++ main.lua
@@ -1,8 +1,28 @@
+--function love.load()
+--    video = love.graphics.newVideo("assets/man-driving-automobile.ogv")
+--    video:play()
+--end
+
+--function love.draw()
+--    love.graphics.draw(video, 0, 0)
+--end
+
+-- require and initial setup libraries
+SSM = require "lib.StackingSceneMgr".newManager()
+SSM.setPath("scenes/")
+
+-- require and initial setup utility files
+require "obstacles"
+require "player"
+
 function love.load()
-    video = love.graphics.newVideo("assets/man-driving-automobile.ogv")
-    video:play()
+  SSM.add("testscene")
+end
+
+function love.update(dt)
+  
 end
 
 function love.draw()
-    love.graphics.draw(video, 0, 0)
+  SSM.draw()
 end
---
+++ scenes/testscene.lua
@@ -0,0 +1,14 @@
+local scene = {}
+
+function scene.load()
+end
+
+function scene.update(dt)
+  
+end
+
+function scene.draw()
+  love.graphics.rectangle("fill", 0, 0, 200, 200)
+end
+
+return scene
